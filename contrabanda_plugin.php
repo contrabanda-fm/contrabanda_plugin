51e7036f7cc52f2ce245eab81df4fed881ae191c
bacterio <albert.caballero.gordillo@gmail.com>
2022-03-26 19:29:27 +0100

    Create main file and lib folder

---
+++ contrabanda_plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Contrabanda
+ * Plugin URI:        https://github.com/contrabanda-fm/contrabanda_plugin
+ * Description:       A wp plugin to handle the radio schedule and programs.
+ * Version:           1.0.0
+ * Author:            Bacterio
+ * Text Domain:       contrabanda
+ * Domain Path:       /languages
+ */
+
+//Registrar programes cpt
+
+//Registrar custom fields de programes
+//Registrar custom fields de la graella
+//Display graella
+//Display programes
+
+
+ ?>
